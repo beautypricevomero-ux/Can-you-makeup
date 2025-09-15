bda0dbf9673875f3a8315c149001a853fc1022d6
beautypricevomero-ux <beautypricevomero@gmail.com>
2025-09-16 01:36:57 +0200

    Create page.tsx

---
+++ app/play/page.tsx
@@ -0,0 +1,8 @@
+export default function Play() {
+  return (
+    <section>
+      <h1 style={{fontSize:"24px", fontWeight:600}}>Gioco</h1>
+      <p>Qui apparirà lo swipe stile Tinder. (Stub)</p>
+    </section>
+  );
+}
